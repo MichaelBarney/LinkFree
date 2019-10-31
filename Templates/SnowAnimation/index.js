a852bc63f8a928b509937e8098212e4c5ccf99c3
Caio Gomes <caiogomes34@gmail.com>
2019-10-31 19:42:06 -0300

    feat: adding snow flakes animation template

---
+++ Templates/SnowAnimation/index.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+     // Add here commands to run on page loaded
+});
+
+$( ".link" ).click(function() {
+    // Add here commands to run on link click
+});
+
+particlesJS.load('particles-js', 'particles.json', function() {
+    console.log('callback - particles.js config loaded');
+});
